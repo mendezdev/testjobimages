3bb2f963886da5b2443dd3744df52094c19e4f3c
Pablo Mendez <pablo.mendez@mercadolibre.com>
2020-10-10 17:28:55 -0300

    added base model db

---
+++ AgileEngineImages/AgileEntineImages.Domain.Common/BaseEntityModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace AgileEntineImages.Domain.Common
+{
+    public abstract class BaseEntityModel<T> : IIdentity<T> where T : class
+    {
+        public  BaseEntityModel()
+        {
+        }
+
+        public string id { get; set; }
+    }
+}
---
+++ AgileEngineImages/AgileEntineImages.Domain.Common/IIdentity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace AgileEntineImages.Domain.Common
+{
+    public interface IIdentity<T>
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        string id { get; set; }
+    }
+}
