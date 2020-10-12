dab167a901c8adec2f0b4e388ed97aa5d863f737
Pablo Mendez <mendez.developer@gmail.com>
2020-10-12 19:54:51 -0300

    improved logic to search (in progress)

---
+++ AgileEngineImages/AgileEngiteImages.ApplicationServices/Mappers/ImageMapper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using AgileEngineImages.Domain.Entities;
+using AgileEngiteImages.ApplicationServices.DTO;
+
+namespace AgileEngiteImages.ApplicationServices.Mappers
+{
+    public class ImageMapper : IMapper<Image, ImageResponseDto>
+    {
+        public Image From(ImageResponseDto obj)
+        {
+            return new Image
+            {
+                Id = obj.Id,
+                Author = obj.Author,
+                Camera = obj.Camera,
+                Tags = obj.Tags,
+                CroppedPicture = obj.Cropped_Picture,
+                FullPicture = obj.Full_Picture
+            };
+        }
+    }
+}
